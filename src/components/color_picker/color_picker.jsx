bab2cee08f01c2e349333a7eca3ad2c953912ca8
Bryce Schultz <bryceschultz@live.com>
2024-04-19 16:12:04 -0700

    fixed warning

--- src/components/color_picker/color_picker.jsx
+++ src/components/color_picker/color_picker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './color_picker.css';
 
 export default function ColorPicker({ value, onChange })
